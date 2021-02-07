eb6e4faa3feddc00a0bd92631919a3b280b6543b
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-02-07 23:54:39 +0300

    fixed bug on the div switch display functionality.

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     // UI declaration
-    const game_home = document.querySelector('#gameHome');
-    const all_games = document.querySelector('all_games_btn')
+    
+    const all_games = document.querySelector('#all_games_btn')
     const open_game = document.querySelectorAll('.open_btns')
 
 
     // adding eventLisners
     all_games.addEventListener('click',displayHome)
-    open_game.addEventListener('click', gameDisplay)
+
+    open_game.forEach(btn => {
+        btn.addEventListener('click', gameDisplay)
+    })
     
 })
 
 
+
 function displayHome() {
+    const game_home = document.querySelector('#gameHome');
     game_home.style.display = 'block';
-    game1.style.display = 'none';
+    document.querySelector('#game1').style.display = 'none';
 
 }
 
-function gameDisplay(e) {
-    const game = document.querySelector(`#${e.target.dataset.game_id}`)
+function gameDisplay(game) {
+    const game_home = document.querySelector('#gameHome');
     game_home.style.display = 'none';
     game.style.display = 'block'
 }
