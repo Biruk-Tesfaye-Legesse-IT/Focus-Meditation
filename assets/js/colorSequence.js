7f525c116c7d45d226f7a8cdb20752cdb59817ee
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2021-02-13 14:46:22 +0300

    ColorSequence js started with UI variables.

---
+++ assets/js/colorSequence.js
@@ -0,0 +1,17 @@
+// jshint esversion:6
+
+
+
+const turnCounter = document.querySelector("#turn");
+const top1 = document.querySelector("#top1");
+const top2 = document.querySelector("#top2");
+const top3 = document.querySelector("#top3");
+const bottom1 = document.querySelector("#bottom1");
+const bottom2 = document.querySelector("#bottom2");
+const bottom3 = document.querySelector("#bottom3");
+const bbottom1 = document.querySelector("#b-bottom1");
+const bbottom2 = document.querySelector("#b-bottom2");
+const bbottom3 = document.querySelector("#b-bottom3");
+const startButton = document.querySelector("#start");
+
+
