16ccf67bc068580067d0589e2f6bdb1d885879cf
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2021-02-13 19:33:25 +0300

    play function added.

--- assets/js/colorSequence.js
+++ assets/js/colorSequence.js
@@ -1,6 +1,15 @@
 // jshint esversion:6
 
 
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let win;
+
 
 const turnCounter = document.querySelector("#turn");
 const top1 = document.querySelector("#top1");
@@ -14,4 +23,30 @@ const bbottom2 = document.querySelector("#b-bottom2");
 const bbottom3 = document.querySelector("#b-bottom3");
 const startButton = document.querySelector("#start");
 
+
+startButton.addEventListener('click', (event) => {
+    if (on || win) {
+        play();
+    }
+});
+
+function play() {
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    turnCounter.innerHTML = 1;
+    good = true;
+
+    for (var i = 0; i < 20; i++) {
+        order.push(Math.floor(Math.random() * 9) + 1);
+
+    }
+    console.log(order);
+    compTurn = true;
+
+    intervalId = setInterval(gameTurn, 800);
+}
 
